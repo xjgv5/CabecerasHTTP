96f4231975e7d1c21aa008b2619aa7a39d1f2bbe
Javier Gomez <javier.gov@outlook.es>
2024-01-01 23:45:53 -0600

    Inicio de proyecto

---
+++ src/main/java/com/xjgv/apiservlet/webapp/headers/CabecerasHttpRequest.java
@@ -0,0 +1,8 @@
+package com.xjgv.apiservlet.webapp.headers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/cabeceras-request")
+public class CabecerasHttpRequest extends HttpServlet{
+}
