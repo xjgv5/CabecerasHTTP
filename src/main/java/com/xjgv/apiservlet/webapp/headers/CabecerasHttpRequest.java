6a57d8896f4332a01c1374b695e25a27c6da6c14
Javier Gomez <javier.gov@outlook.es>
2024-01-02 13:10:40 -0600

    cabeceras http del request

--- src/main/java/com/xjgv/apiservlet/webapp/headers/CabecerasHttpRequest.java
+++ src/main/java/com/xjgv/apiservlet/webapp/headers/CabecerasHttpRequest.java
@@ -1,8 +1,45 @@
 package com.xjgv.apiservlet.webapp.headers;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/cabeceras-request")
-public class CabecerasHttpRequest extends HttpServlet{
+public class CabecerasHttpRequest extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+
+        String metodoHttp = req.getMethod();
+        String requestUri = req.getRequestURI();
+        String requestURL = req.getRequestURL().toString();
+        String contextPath = req.getContextPath();
+        String servletPath = req.getServletPath();
+        try (PrintWriter out = resp.getWriter()) {
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("     <head>");
+            out.println("         <meta charset=\" UTF-8\">");
+            out.println("         <title>Cabeceras HTTP Request</title>");
+            out.println("     </head>");
+            out.println("     <body>");
+            out.println("         <h1>Cabeceras HTTP Request</h1>");
+            out.println("         <ul>");
+            out.println("               <li>Método http: " + metodoHttp + "</li>");
+            out.println("               <li>Request URI: " + requestUri + "</li>");
+            out.println("               <li>Request URL: " + requestURL + "</li>");
+            out.println("               <li>Context Path: " + contextPath + "</li>");
+            out.println("               <li>Servleth Path: " + servletPath + "</li>");
+            out.println("         </ul>");
+            out.println("     </body>");
+            out.println("</html>");
+        }
+    }
 }
