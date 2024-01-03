6e01d2b275cb51641d6bac2086f7f15ec3151d3f
Javier Gomez <javier.gov@outlook.es>
2024-01-03 01:14:39 -0600

    enviando un objeto json al cuerpo del request

--- src/main/java/com/xjgv/apiservlet/webapp/headers/controllers/ProductoJsonServlet.java
+++ src/main/java/com/xjgv/apiservlet/webapp/headers/controllers/ProductoJsonServlet.java
@@ -5,12 +5,14 @@ import com.xjgv.apiservlet.webapp.headers.models.Producto;
 import com.xjgv.apiservlet.webapp.headers.services.ProductoService;
 import com.xjgv.apiservlet.webapp.headers.services.ProductoServiceImpl;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,5 +29,32 @@ public class ProductoJsonServlet extends HttpServlet {
         resp.setContentType("application/json");
         resp.getWriter().write(json);
 
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletInputStream jsonStream = req.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        Producto producto = mapper.readValue(jsonStream, Producto.class);
+        resp.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = resp.getWriter()) {
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("     <head>");
+            out.println("         <meta charset=\" UTF-8\">");
+            out.println("         <title>Detalle de producto desde json</title>");
+            out.println("     </head>");
+            out.println("     <body>");
+            out.println("         <h1>Detalle de producto desde json</h1>");
+            out.println("         <ul>");
+            out.println("           <li>ID: " + producto.getId() + "</li>");
+            out.println("           <li>Nombre: " + producto.getNombre() + "</li>");
+            out.println("           <li>Tipo: " + producto.getTipo() + "</li>");
+            out.println("           <li>Precio: " + producto.getPrecio() + "</li>");
+            out.println("         </ul>");
+            out.println("     </body>");
+            out.println("</html>");
+        }
     }
 }
